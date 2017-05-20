ad801a531393c1c43da72089f7fe8c3c9cc678ec
Estefania Cass <TefiC@users.noreply.github.com>
2017-05-20 17:30:49 +0000

    Added modals forms  and edit functionality

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.css';
 import {reactLocalStorage} from 'reactjs-localstorage';
-import { Modal, Button, FormControl } from 'react-bootstrap/lib';
+import { Modal, Button, FormControl, FormGroup, ControlLabel } from 'react-bootstrap/lib';
 import PropTypes from 'prop-types';
 
 var MainComponent = React.createClass({
@@ -62,11 +62,26 @@ var RecipeContainer = React.createClass({
 	recipeIngredients: PropTypes.array,
 	recipeId: PropTypes.number,
 	
+	getInitialState: function() {
+		return {
+			'recipeName': this.props.recipeName,
+			'recipeIngredients': this.props.recipeIngredients
+		}	
+	},
+	
+	updateContainer: function(state) {
+		/*
+		 * state is a dictionary
+		 */
+		console.log(state);
+		this.setState(state);	
+	},
+	
 	render: function() {
 		return (
 			<div>
-				<RecipeHeader recipeName={this.props.recipeName} recipeId={this.props.recipeId}/>
-				<RecipeBody recipeIngredients={this.props.recipeIngredients} recipeId={this.props.recipeId}/>
+				<RecipeHeader recipeName={this.state.recipeName} recipeId={this.props.recipeId}/>
+				<RecipeBody recipeName={this.state.recipeName} recipeIngredients={this.state.recipeIngredients} recipeId={this.props.recipeId} onUpdateContainer={this.updateContainer}/>
 			</div>
 		);
 	}
@@ -76,6 +91,7 @@ var RecipeHeader = React.createClass({
 	
 	recipeId: PropTypes.string,
 	recipeName: PropTypes.string,
+	onUpdateContainer: PropTypes.func,
 	
 	handleUserClick: function() {
 		
@@ -91,8 +107,10 @@ var RecipeHeader = React.createClass({
 
 var RecipeBody = React.createClass({
 	
+	recipeName: PropTypes.string,
 	recipeIngredients: PropTypes.array,
 	recipeId: PropTypes.number,
+	// onUpdateContainer: PropTypes.func,
 	
 	getInitialState: function() {
 		return {
@@ -101,14 +119,27 @@ var RecipeBody = React.createClass({
 		};	
 	},
 	
-	toggleEditModal: function() {
+	toggleEditModal: function(state) {
 		var newState = !this.state.showEditModal;
+		this.updateContainer(state);
 		this.setState({showEditModal: newState});
+	},
+	
+	updateContainer: function(state) {
+		this.props.onUpdateContainer(state);
 	},
 	
 	toggleDeleteModal: function() {
 		var newState = !this.state.showDeleteModal;
 		this.setState({showDeleteModal: newState});
+		// this.props.onUpdateContainer(state);
+	},
+	
+	getRecipeProperties: function() {
+		return {
+			'name': this.props.recipeName,
+			'ingredients': this.props.recipeIngredients
+		}
 	},
 	
 	render: function() {
@@ -118,7 +149,7 @@ var RecipeBody = React.createClass({
 				<RecipeBodyList recipeIngredients={this.props.recipeIngredients}/>
 				<RecipeBodyButton buttonStyle="primary" buttonTitle="Edit" onModalToggle={this.toggleEditModal}/>
 				<RecipeBodyButton buttonStyle="primary" buttonTitle="Delete" onModalToggle={this.toggleDeleteModal}/>
-				<RecipeEditModal show={this.state.showEditModal} onHide={this.toggleEditModal} recipeId={this.props.recipeId.toString()} />
+				<RecipeEditModal show={this.state.showEditModal} onHide={this.toggleEditModal} recipeId={this.props.recipeId.toString()} recipeProperties={this.getRecipeProperties()}/>
 				<RecipeDeleteModal show={this.state.showDeleteModal} onHide={this.toggleDeleteModal} recipeId={this.props.recipeId.toString()} />
 			</div>
 		);
@@ -149,7 +180,7 @@ var RecipeBodyButton = React.createClass({
 	render: function() {
 		
 		if (this.props.buttonTitle == "Edit" || this.props.buttonTitle == "Add recipe" || this.props.buttonTitle == "Delete") {
-			return <button dsStyle={this.props.buttonStyle} onClick={this.props.onModalToggle}>{this.props.buttonTitle}</button>;
+			return <Button dsStyle={this.props.buttonStyle} onClick={this.props.onModalToggle}>{this.props.buttonTitle}</Button>;
 		}
 	}
 });
@@ -163,22 +194,58 @@ var RecipeIngredient = React.createClass({
 	}
 });
 
-
+// -------------------------------------------------
 // MODALS 
+// -------------------------------------------------
 
 var RecipeAddModal = React.createClass({
 	
 	show: PropTypes.bool,
 	onHide: PropTypes.func,
-
+	
+	getInitialState: function() {
+		return {
+			'name': '',
+			'ingredients': ''
+		};
+	},
+	
+	handleChange: function() {
+		
+		this.setState({'name': this.inputName.value, 'ingredients': this.inputIngredients.value});
+	},
+	
 	render: function() {
 		return (
 			<Modal show={this.props.show} onHide={this.props.onHide}>
 				<Modal.Header closeButton>
-					<Modal.Title>Modal heading</Modal.Title>
+					<Modal.Title>Add recipe</Modal.Title>
 				</Modal.Header>
 				<Modal.Body>
-					<h4>Add recipe</h4>
+					<form>
+						<ControlLabel>
+							New Recipe:
+						</ControlLabel>
+						<FormGroup>
+							<FormControl 
+								inputRef={(input) => this.inputName = input}
+								type="text"
+								value={this.state.name}
+								placeholder= "Enter New Recipe"
+								onChange={this.handleChange} />
+						</FormGroup>
+						<ControlLabel>
+							Ingredients for this recipe:
+						</ControlLabel>
+						<FormGroup>
+							<FormControl 
+								inputRef={(input) => this.inputIngredients = input}
+								componentClass="textarea"
+								value={this.state.ingredients}
+								placeholder= "Enter Ingredients"
+								onChange={this.handleChange} />
+						</FormGroup>
+					</form>
 				</Modal.Body>
 				<Modal.Footer>
 					<Button onClick={this.props.onHide}>Close</Button>
@@ -193,19 +260,82 @@ var RecipeEditModal = React.createClass({
 	show: PropTypes.bool,
 	onHide: PropTypes.func,
 	recipeId: PropTypes.number,
-
+	state: PropTypes.object,
+	recipeProperties: PropTypes.object,
+	
+	getInitialState: function() {
+		return {
+			'recipeName': this.props.recipeProperties.name,
+			'recipeIngredients': this.props.recipeProperties.ingredients
+		}
+	},
+	
+	handleChange: function() {
+		this.setState({'recipeName': this.inputName.value, 'recipeIngredients': this.inputIngredients.value});	
+	},
+	
+	handleSubmit: function() {
+		
+		//TODO: MAKE A DICT WITH AN INGREDIENTS ARRAY
+		if (this.state.recipeName != '' && this.state.recipeIngredients != '') {
+			
+			var ingredientsArray = this.formatIngredients(this.state.recipeIngredients);
+			
+			var state = {
+				'recipeName': this.state.recipeName,
+				'recipeIngredients': ingredientsArray
+			}
+		
+			this.props.onHide(state);
+		} else {
+			alert("You can't set an empty recipe")
+		}
+		
+	},
+	
+	handleClose: function() {
+		this.props.onHide(this.getInitialState());	
+	},
+	
+	formatIngredients: function(ingredientsString) {
+		var array = ingredientsString.split(',');
+		return array;
+	},
+	
 	render: function() {
 		return (
-			<Modal show={this.props.show} onHide={this.props.onHide}>
+			<Modal show={this.props.show} onHide={this.handleClose}>
 				<Modal.Header closeButton>
-					<Modal.Title>Modal heading</Modal.Title>
+					<Modal.Title>Edit recipe</Modal.Title>
 				</Modal.Header>
 				<Modal.Body>
-					<h4>Edit data</h4>
-					<p>Modal id {this.props.recipeId} </p>
+					<form>
+						<ControlLabel>
+							Edit recipe name:
+						</ControlLabel>
+						<FormGroup>
+							<FormControl 
+								inputRef={(input) => this.inputName = input}
+								type="text"
+								value={this.state.recipeName}
+								placeholder= "Edit recipe name"
+								onChange={this.handleChange} />
+						</FormGroup>
+						<ControlLabel>
+							Edit ingredients for this recipe:
+						</ControlLabel>
+						<FormGroup>
+							<FormControl 
+								inputRef={(input) => this.inputIngredients = input}
+								componentClass="textarea"
+								value={this.state.recipeIngredients}
+								placeholder= "Edit Ingredients"
+								onChange={this.handleChange} />
+						</FormGroup>
+					</form>
 				</Modal.Body>
 				<Modal.Footer>
-					<Button onClick={this.props.onHide}>Close</Button>
+					<Button onClick={this.handleSubmit}>Change</Button>
 				</Modal.Footer>
 			</Modal>
 		);
